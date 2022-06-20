7b4295650dec4364084db2c75bf8b4a53a99f7d2
Anushka Samaranayake <sandaruwan.samaranayake@primelab.io>
2022-06-21 00:13:01 +0530

    Adding Erasure Code Decoding

--- src/fileshare/ErasureCodingDecode.go
+++ src/fileshare/ErasureCodingDecode.go
@@ -1,1 +1,110 @@
 package fileshare
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/klauspost/reedsolomon"
+)
+func erasureDecoding(dataShards int, parShards int,localStorage string,fileName string){
+
+
+	// Create matrix
+	enc, err := reedsolomon.NewStream(dataShards, parShards)
+	checkErr(err)
+
+	// Open the inputs
+	shards, size, err := openInput(dataShards, parShards, fileName)
+	checkErr(err)
+
+	// Verify the shards
+	ok, err := enc.Verify(shards)
+	if ok {
+		fmt.Println("No reconstruction needed")
+	} else {
+		fmt.Println("Verification failed. Reconstructing data")
+		shards, size, err = openInput(dataShards, parShards, fileName)
+		checkErr(err)
+		// Create out destination writers
+		out := make([]io.Writer, len(shards))
+		for i := range out {
+			if shards[i] == nil {
+				outfn := fmt.Sprintf("%s.%d", fileName, i)
+				fmt.Println("Creating", outfn)
+				out[i], err = os.Create(outfn)
+				checkErr(err)
+			}
+		}
+		err = enc.Reconstruct(shards, out)
+		if err != nil {
+			fmt.Println("Reconstruct failed -", err)
+			os.Exit(1)
+		}
+		// Close output.
+		for i := range out {
+			if out[i] != nil {
+				err := out[i].(*os.File).Close()
+				checkErr(err)
+			}
+		}
+		shards, size, err = openInput(dataShards, parShards, fileName)
+		ok, err = enc.Verify(shards)
+		if !ok {
+			fmt.Println("Verification failed after reconstruction, data likely corrupted:", err)
+			os.Exit(1)
+		}
+		checkErr(err)
+	}
+
+	// Join the shards and write them
+	outfn := localStorage
+	if outfn == "" {
+		outfn = fileName
+	}
+
+	fmt.Println("Writing data to", outfn)
+	f, err := os.Create(outfn)
+	checkErr(err)
+
+	shards, size, err = openInput(dataShards, parShards, fileName)
+	checkErr(err)
+
+	// We don't know the exact filesize.
+	err = enc.Join(f, shards, int64(dataShards)*size)
+	checkErr(err)
+
+}
+
+func openInput(dataShards, parShards int, fname string) (r []io.Reader, size int64, err error) {
+	// Create shards and load the data.
+	shards := make([]io.Reader, dataShards+parShards)
+	for i := range shards {
+		infn := fmt.Sprintf("%s.%d", fname, i)
+		fmt.Println("Opening", infn)
+		f, err := os.Open(infn)
+		if err != nil {
+			fmt.Println("Error reading file", err)
+			shards[i] = nil
+			continue
+		} else {
+			shards[i] = f
+		}
+		stat, err := f.Stat()
+		checkErr(err)
+		if stat.Size() > 0 {
+			size = stat.Size()
+		} else {
+			shards[i] = nil
+		}
+	}
+	return shards, size, nil
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		os.Exit(2)
+	}
+}
